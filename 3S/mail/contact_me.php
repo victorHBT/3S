b556db22007fb4746185949eaebb0cb901a45dfb
victorHBT <30874598+victorHBT@users.noreply.github.com>
2020-06-16 22:05:50 -0300

    commit
    mudanças nas tegs de cadastro no contact_me.php

--- 3S/mail/contact_me.php
+++ 3S/mail/contact_me.php
@@ -16,14 +16,14 @@ if(empty($_POST['name'])      ||
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$phone = strip_tags(htmlspecialchars($_POST['consumo']));
-$phone = strip_tags(htmlspecialchars($_POST['local']));
+$consumo = strip_tags(htmlspecialchars($_POST['consumo']));
+$local = strip_tags(htmlspecialchars($_POST['local']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = '3ssolarsolucoes@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nConsumo: $consumo\n\nLocal: $local\n\nMessage:\n$message";
 $headers = "From: 3ssolarsolucoes@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
